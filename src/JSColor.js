a92925667a6d155d2738ec047094a8d0c027d8f3
Yuichi Tateno <hotchpotch@gmail.com>
2008-12-12 16:52:06 +0900

    syntax error

--- src/JSColor.js
+++ src/JSColor.js
@@ -1,236 +1,262 @@
 
-JSColor = function() {};
-
-JSColor.Util = {
-    getHSB: function(uint) {
-        var rgb = this.getRGB(uint);
-        return this.RGB2HSB(rgb.r, rgb.g, rgb.b);
-    },
-    getHLS: function(uint) {
-        var rgb = this.getRGB(uint);
-        return this.RGB2HLS(rgb.r, rgb.g, rgb.b);
-    },
-    getRGB: function(uint) {
-        return {
-            r: uint >> 16 & 0xFF,
-            g: uint >> 8  & 0xFF,
-            b: uint       & 0xFF
-        }
-    },
-    setHSB: function(rgb, h, s, b) {
-        var t = this.HSB2RGB(h, s, b);
-        this.setRGB(rgb, t.r, t.g, t.b);
-        return rgb;
-    },
-    setHLS:  function(rgb, h, l, s) {
-        var t = this.HLS2RGB(h, l, s);
-        this.setRGB(rgb, t.r, t.g, t.b);
-        return rgb;
-    },
-    setRGB: function(rgb, r, g, b) {
-        r = (r < 0) ? 0 : (r > 255) ? 255 : Math.round(r);
-        g = (g < 0) ? 0 : (g > 255) ? 255 : Math.round(g);
-        b = (b < 0) ? 0 : (b > 255) ? 255 : Math.round(b);
-        rgb.r = r << 16;
-        rgb.g = g << 8;
-        rgb.b = b;
-        return rgb;
-    },
-    HLS2HSB: function(h, l, s) {
-        var rgb = this.HLS2RGB(h, l, s);
-        return 
-    },
-    HLS2RGB: function(h, l, s) {
-        var max, min;
-        h = (h < 0) ? (h % 360 + 360) : (h >= 360 ? h % 360 : h);
-        l = (l < 0) ? 0 : (l > 100) ? 100 : l;
-        s = (s < 0) ? 0 : (s > 100) ? 100 : s;
-
-        l *= 0.01;
-        s *= 0.01;
-
-        if (s == 0) {
-            var val = l * 255;
-            return {
-                r: val,
-                g: val,
-                b: val
-            }
-        }
-
-        if (l < 0.5) {
-            max = l * (1 + s) * 255
-        } else {
-            max = (l * (1 - s) + s) * 255
-        }
-        min = (2 * l) * 255 - max;
-
-        return this._hMinMax2RGB(h, min, max);
-    },
-    HSB2HLS: function(h, s, b) {
-        var rgb = this.HSB2RGB(h, s, b);
-        return this.RGB2HLS(rgb.r, rgb.g, rgb.b);
-    },
-    HSB2RGB: function(hue, sat, bri) {
-        hue = (hue<0)? hue % 360+360 : (hue>=360)? hue%360: hue;
-        sat = (sat<0)? 0 : (sat>100)? 100: sat;
-        bri = (bri<0)? 0 : (bri>100)? 100: bri;     
-
-        sat *= 0.01;
-        bri *= 0.01;
-
-        if(sat == 0){
-            var val = bri*255;
-            return {r:val, g:val, b:val};
-        }
-
-        var max = bri*255;
-        var min = max*(1-sat);
-
-        return this._hMinMax2RGB(hue, min, max);
-    },
-    RGB2HSB: function(r, g, b) {
-        r = (r < 0)? 0 : (r>255)? 255: Math.round(r);
-        g = (g < 0)? 0 : (g>255)? 255: Math.round(g);
-        b = (b < 0)? 0 : (b>255)? 255: Math.round(b);
-
-        var min = Math.min(r, g, b);
-        var max = Math.max(r, g, b);
-        var sat;
-
-        //saturation
-        if (max==0) {
-            return {h:0, s: 0, b: 0}
-        } else {
-            sat = (max - min)/max * 100;
-        }
-
-        var bri = max / 255 * 100;
-
-        var hue = this._getHue(r, b, g, max, min);
-        return {h:hue, s:sat, b:bri}
-    },
-    RGB2HLS: function(r, g, b) {
-        r = (r < 0)? 0 : (r>255)? 255: Math.round(r);
-        g = (g < 0)? 0 : (g>255)? 255: Math.round(g);
-        b = (b < 0)? 0 : (b>255)? 255: Math.round(b);
-
-        var min = Math.min(r,g,b);
-        var max = Math.max(r,g,b);
-        var l = (max + min)*0.5 / 255 * 100;
-
-        var dist = (max - min);
-        var h;
-        var s;
-        if(dist==0){
-            h = 0;
-            s = 0;
-        }else{
-            if( l < 127.5){
-                s = dist/(max+min)*100;
-            }else{
-                s = dist/(510-max-min)*100;
-            }
-
-            h = 360 - this._getHue(r,g,b,max, min);
-        }
-
-        return {h:h, l:l, s:s}
-    },
-    getLuminous: function(color) {
-        var r = color >> 16 & 0xff;
-        var g = color >> 8 & 0xff;
-        var b = color & 0xff;
-
-        var min = Math.min(r,g,b);
-        var max = Math.max(r,g,b);
-        var l = (max + min)*0.5 / 255 * 100;
-
-
-        return l;
-    },
-    _hMinMax2RGB: function(h, min, max) {
-        var r,g,b;
-        var area = Math.floor(h / 60);
-        switch (area) {
-            case 0:
-                r = max;
-                //0 - 0, 60-255
-                g = min+h * (max-min)/ 60;
-                b = min;
-                break;
-            case 1:
-                r = max-(h-60) * (max-min)/60;
-                g = max;
-                b = min;
-                break;
-            case 2:
-                r = min ;
-                g = max;
-                b = min+(h-120) * (max-min)/60;
-                break;
-            case 3:
-                r = min;
-                g = max-(h-180) * (max-min)/60;
-                b = max;
-                break;
-            case 4:
-                r = min+(h-240) * (max-min)/60;
-                g = min;
-                b = max;
-                break;
-            case 5:
-                r = max;
-                g = min;
-                b = max-(h-300) * (max-min)/60;
-                break;
-            case 6:
-                r = max;
-                //0 - 0, 60-255
-                g = min+h  * (max-min)/ 60;
-                b = min;
-                break;
-        }
-
-        r = Math.min(255, Math.max(0, Math.round(r)));
-        g = Math.min(255, Math.max(0, Math.round(g)));
-        b = Math.min(255, Math.max(0, Math.round(b)));
-
-        return {
-            r: r, 
-            g: g, 
-            b: b
-        };
-    },
-    _getHue: function(r, g, b, max, min) {
-        var range = max - min;
-        if (range==0){
-            return 0;
-        }
-
-        var rr = (max - r);
-        var gg = (max - g);
-        var bb = (max - b);
-
-        var h;
-        switch(max){
-            case r:
-                h = bb - gg;
-                break;
-            case g:
-                h = 2 *range+ rr - bb;
-                break;
-            case b:
-                h = 4 *range+ gg - rr;
-                break;
-        }
-
-        h*=-60;
-        h/=range;
-        h = (h<0)? h+360: h;
-
-        return h;
-    }
+JSColor = function() {
+    return function() {
+      this.initialize.apply(this, arguments);
+    };
 }
 
+JSColor.createRGB = function(red, green, blue) {
+    var col = new JSColor();
+    col.setRGB(red, green, blue);
+    return col;
+}
+    
+JSColor.createHSB = function(hue, saturation, brightness) {
+   var col = new JSColor();
+   col.setHSB(hue, saturation, brightness);
+   return col;
+}
+    
+JSColor.createGray = function(gray) {
+    var col = new JSColor();
+    col.setRGB(gray, gray, gray);
+    return col;
+}
+    
+JSColor.prototype = function() {
+    initialize: function(color) {
+        this._value = Math.max(0, parseInt(color || 0));
+        
+        this._red = this._green = this._blue = this._hue = this._saturation = this._brightness = 0;
+        this._hsbUpdate = this._rgbUpdate = true;
+        this._holdHue = this._holdSaturation = false;
+    },
+        
+    /**
+     * Set color with red, gree, bule.
+     * 
+     * @param red 0-255;
+     * @param green 0-255;
+     * @param blue 0-255;
+     */
+    setRGB: function(r, g, b) {
+        r = Math.min(255, Math.max(0, r));
+        g = Math.min(255, Math.max(0, g));
+        b = Math.min(255, Math.max(0, b));
+        
+        this.setValue((r << 16) | (g << 8 ) | b);
+    },
+    
+    
+    /**
+     * 色相(hue), 彩度(saturation), 明度(brightnessChanges)から色を定義します。
+     * 
+     * @param hue 0-360;
+     * @param saturation 0-100;
+     * @param brightness 0-100;
+     */
+    setHSB: function(hue, saturation, brightness) {
+        var rgbInfo = JSColor.Util.HSB2RGB(hue,saturation,brightness);
+        this.setRGB(rgbInfo.r, rgbInfo.g, rgbInfo.b);
+    },
+    
+    
+    /**
+     * Returns copy of instance.
+     * 
+     * @return The copy of the Color instance;
+     */
+    clone: function() {
+        return new JSColor(this.getValue());
+    },
+    
+    
+    /**
+     * Hex color value 0xRRGGBB;
+     */
+    getValue: function() {
+        return this._value;
+    },
+    
+    setValue: function(color) {
+        if(_value == color) return;
+        this._value = color;
+        this._hsbUpdate = true;
+        this._rgbUpdate = true;
+        if (typeof this.callbackChangeValue == 'function')
+            this.callbackChangeValue();
+    },
+    
+    
+    /**
+     * Returns 32bit color, 0xAARRGGBB;
+     * @returns int;
+     */
+    getValue32: function(alpha) {
+        alpha = parseInt(alpha*255);
+        var val = (alpha<<24) | value;
+        return val;
+    },
+    
+    
+    
+    /*
+    ---------------------------------------------
+    RGB getter / setter;
+    ---------------------------------------------
+    */
+    
+    getRed: function() { 
+        this.updateRGB();
+        return this._red;
+    },
+    
+    getGreen: function() {
+        this.updateRGB();
+        return this._green;
+    },
+    
+    getBlue: function() {
+        this.updateRGB();
+        return this._blue;
+    },
+    
+    setRed: function(val) {
+        if (val!=this._red)
+            this.setRGB(this._val,this.getGreen(),this.getBlue());
+    },
+    
+    setGreen: function(val) {
+        if (val!=this._green)
+            this.setRGB(this.getRed(),val,this.getBlue());
+    },
+    
+    setBlue: function(val) {
+        if (val!=this.blue)
+            this.setRGB(this.getRed(),this.getGreen(),val);
+    },
+    
+    
+    /**
+     * Returns Sring expression of this color. "RRGGBB"
+     * @returns String;
+     */
+    toStringRGB: function():String {
+        var hexTable = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+        return [
+          hexTable[Math.floor(this.getRed() / 16)],
+          hexTable[this._red%16],
+          hexTable[Math.floor(this.getGreen() / 16)],
+          hexTable[this._green%16],
+          hexTable[Math.floor(this.getBlue() / 16)],
+          hexTable[this._blue%16]
+        ].join('');
+    },
+    
+    toString: function() {
+        return "0x" + this.toStringRGB();
+    }
+    
+    
+    /*
+    ------------------------------------------------
+    HSB getter / setter;
+    ------------------------------------------------
+    */
+    
+    getHue: function() {
+        this.updateHSB();
+        return this._hue;
+    },
+    
+    getSaturation: function() {
+        this.updateHSB();
+        return this._saturation;
+    },
+    
+    getBrightness: function() {
+        this.updateHSB();
+        return this._brightness;
+    },
+    
+    setHue: function(val) {
+        var rgbInfo = JSColor.Util.HSB2RGB(val,saturation,brightness);
+        this.setRGB(rgbInfo.r, rgbInfo.g, rgbInfo.b);
+        this._hue = val;
+    },
+    
+    setSaturation: function(val) {
+        var rgbInfo = JSColor.Util.HSB2RGB(hue,val,brightness);
+        this.setRGB(rgbInfo.r, rgbInfo.g, rgbInfo.b);
+        this._saturation = val;
+    },
+    
+    setBrightness: function(val) {
+        //明度が０の場合は、hue,saturationの値を保持する
+        var rgbInfo = JSColor.Util.HSB2RGB(hue,saturation,val);
+        this.setRGB(rgbInfo.r, rgbInfo.g, rgbInfo.b);
+        this._brightness = val;
+    },
+    
+    /*
+    ----------------------------------------------------------------
+    gray scale controll;
+    ----------------------------------------------------------------
+    */
+    
+    /**
+     * 0-255の値でグレーの色を定義します。
+     * 
+     * @param value 0-255 gray scale.
+     */
+    setGray: function(value) {
+        this.setRGB(value,value,value);
+    },
+    
+    
+    /**
+     * 現在の色をグレースケールに変換します。
+     */
+    toGray: function() {
+        //var rw = 0.3086;
+        //var gw = 0.6094;
+        //var bw = 0.0820;
+        //var mat:ColorMatrixFilter = new ColorMatrixFilter([rw,gw,bw,0,0, rw,gw,bw,0,0, rw,gw,bw,0,0, 0,0,0,1,0]);
+        //applyColorMatrix(mat);
+    },
+    
+    
+    
+    
+    /*
+    ------------------------------------------------
+    Internal Use Only;
+    ------------------------------------------------
+    */
+    
+    updateRGB: function() {
+        if(!this._rgbUpdate) return;
+        this._red= this._value>>16 & 0xff;
+        this._green= this._value>> 8 & 0xff;
+        this._blue= this._value& 0xff;
+        this._rgbUpdate = false;
+    },
+    
+    updateHSB: function() {
+        if(!this._hsbUpdate) return;
+        
+        var hsb = JSColor.Util.RGB2HSB(this.getRed,this.getGreen(),this.getBlue());
+        
+        //彩度０、明度が０の場合は昔の情報を保持
+        if(hsb.b != 0 && hsb.s != 0 )
+            this._hue = hsb.h;
+        
+        //明度が０の場合は昔の値を保持
+        if(hsb.b != 0)
+            this._saturation = hsb.s;
+        
+        this._brightness = hsb.b;
+        this._hsbUpdate = false;
+    }
+}
 
